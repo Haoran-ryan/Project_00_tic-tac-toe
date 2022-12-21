1398ba6da7d9b1ef3044b966cec1c622371e8ab7
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2022-12-22 00:06:41 +1100

    interactions added; restartGame is still bit buggy

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 # Project 1 : Tic Tac Toe
 
 ## Logs
-<u>**19-Dec-2022**</u>
+#### 19-Dec-2022
 1. basic structure working 
-2. basic winning rules working 
+2. basic winning rules working
+
+#### 20-Dec-2022
+1. all the rules working
+2. restart button working
+<u>**21-Dec-2022**</u>
+1. adding a strike to make the winning result more visible
+
 
 ## Development Ideas 
-<u>**19-Dec-2022**</u>
+#### 19-Dec-2022
 1. pre-set a winning combo by assigning an 'index-like' number to 9 cells
 2. use grid to arrange the 9 cells in 3x3 layout 
 3. record the 'index' of each taken spot by each player and compare the spots with the winning combo 
 
 ## Bugs 
-<u>**19-Dec-2022**</u>
-1. restart button not working
-2. some rules are not working because of the way array was pushed and compared 
+#### 19-Dec-2022
+1. restart button not working - FIXED
+2. some rules are not working because of the way array was pushed and compared -FIXED
+#### 21-Dec-2022
+1. the auto restart after a game ends invokes an infinite loop of the restartGame function. 
 ---
 ### Copyright (c)
-Author: Haoran (Ryan) Guo 
+Author: Ryan Haoran Guo 
--- script.js
+++ script.js
@@ -3,85 +3,168 @@ const playerX = 'X';
 const playerO = 'O';
 //winning combination
 const winningCombo =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+    //rows
+    {combo:[0,1,2], strikeClass: "strike-row-1"},
+    {combo:[3,4,5], strikeClass: "strike-row-2"},
+    {combo:[6,7,8], strikeClass: "strike-row-3"},
+    //columns
+    {combo:[0,3,6], strikeClass: "strike-column-1"},
+    {combo:[1,4,7], strikeClass: "strike-column-2"},
+    {combo:[2,5,8], strikeClass: "strike-column-3"},
+
+    //diagonals
+    {combo:[0,4,8], strikeClass: "strike-diagonal-1"},
+    {combo:[2,4,6], strikeClass: "strike-diagonal-2"},
+
 ];
 
 // record the positions taken by each player
 // the variables have to be global !
 let player = [-100,-101,-102,-103,-104,-105,-106,-107,-108];
 
-
 const cells = document.querySelectorAll('[data-cell]');
 const board = document.querySelector('.container');
 const win=document.querySelector('#winner');
-const winText = document.querySelector('#winning-message');
+const winningMessage = document.querySelector('#winning-message');
 const restartButton = document.querySelector('#restart');
+const playerStatus = document.querySelector("#player");
+
 let playerO_turn = false;
+let clickCounter = 0;
+let winner = false;
+let gameOver = false;
 
+const strike = document.querySelector('.strike');
+const gameOverSound = new Audio("audio/game-over.wav")
+const clickSound = new Audio("audio/click.wav");
+const winnerSound = new Audio("audio/winner.wav")
 
-// function to check the winning
-//player is a global variable!
-
-const winningRule = function(a,b,c,player){
-    let spot1 = player[a];
-    let spot2 = player[b];
-    let spot3 = player[c];
-    if (spot1 === spot2 && spot1 === spot3 && spot2 == spot3){
-        if (spot1 === 1){
-            console.log("Player X wins!")
-        }else{
-            console.log("Player O wins!")
-        }
-    }
-}
-const checkForWinner = function(){
-    for(let i in winningCombo){
-        let [a,b,c] = winningCombo[i]
-        winningRule(a,b,c,player);
+// function to check if it's the end of the game
+const gameEnd = function(){
+    if(clickCounter === 9 || winner === true){
+        gameOver = true;
     }
 }
 
 // function to restart the game
 const restartGame = function(){
-    Array.from(cells).forEach(item => item.dataset.cell = "0");
+    // for (let i=0; i<9; i++){
+    //     cells[i].dataset.taken = "false";
+    //     cells[i].innerText ="";
+    // }
+    Array.from(cells).forEach(item => item.dataset.taken = "false");
+    Array.from(cells).forEach(item => item.innerText = "");
     playerO_turn = false;
-    spotsX = [];
-    spotsO = [];
+    playerStatus.innerText = "Current Player is X";
+    winningMessage.innerText = "Who Wins?"
+    player = [-100,-101,-102,-103,-104,-105,-106,-107,-108];
+    clickCounter = 0;
+    gameover = false;
+
+}
+
+// function to check the winning ; it's a child function for checkForWinner function
+const winningRule = function(a,b,c,player){
+    let spot1 = player[a];
+    let spot2 = player[b];
+    let spot3 = player[c];
+    if (spot1 && (spot1 === spot2 && spot1 === spot3)){
+    // if (spot1 === spot2 && spot1 === spot3 && spot2 == spot3){
+        if (spot1 === 1){
+            winningMessage.innerText = "Player X Wins!";
+            winningMessage.style.color = "black";
+            winningMessage.style.opacity = "1";
+            console.log("Player X wins!")
+            winner = true;
+            winnerSound.play();
+
+        }else{
+            winningMessage.innerText = "Player O Wins!";
+            winningMessage.style.color = "black";
+            winningMessage.style.opacity = "1";
+            console.log("Player O wins!")
+            winner = true;
+            // setInterval(restartGame,250)
+            winnerSound.play();
+        }
+    } else if(Array.from(cells).every((element) => element.dataset.taken !== 'false')){
+        console.log("It's a draw!")
+        playerStatus.innerText = "Game Over!"
+        winningMessage.innerText = "A Draw";
+        gameOverSound.play()
+        // setInterval(restartGame,250)
+    }
+}
+
+// parent function to check the winner
+const checkForWinner = function(){
+    for(let eachCombo of winningCombo){
+        let [a,b,c] = eachCombo.combo;
+        winningRule(a,b,c,player);
+    }
 }
 
 
-// event listener for the game board
-board.addEventListener("click",function(event){
+// function to display the next player
+const hoverText = function(){
+    // remove all hover text
+    cells.forEach((element) => {
+        element.classList.remove("x-hover");
+        element.classList.remove("o-hover");
+    })
+
+    const hoverClass = `${playerO_turn? "o-hover" : "x-hover"}`;
+    cells.forEach((element) => {
+        if(element.innerText ===""){
+            element.classList.add(hoverClass);
+        }
+    })
+
+
+}
+
+
+// function used for the main event listener
+function main(event){
     const clickedCell = event.target;
     // check if the cell is clicked
     if (clickedCell.dataset.taken !== "false"){
-        console.log("it has been taken!")
-    }else {
-        if (!playerO_turn) {
-            clickedCell.innerText = 'X';
-            clickedCell.dataset.taken = "true";
-            playerO_turn = true;
-            let clickedCellValue = parseInt(clickedCell.dataset.cell);
-            player[clickedCellValue] = 1;
-
-            checkForWinner();
-        } else {
-            clickedCell.innerText = 'O';
-            clickedCell.dataset.taken = "true";
-            playerO_turn = false;
-            let clickedCellValue = parseInt(clickedCell.dataset.cell);
-            player[clickedCellValue] = 2;
-
-            checkForWinner();
-        }
+        return;
     }
-})
 
+    if (!playerO_turn) {
+        clickedCell.innerText = playerX;
+        clickedCell.dataset.taken = "true";
+        clickCounter += 1;
+        playerO_turn = true;
+        let clickedCellValue = parseInt(clickedCell.dataset.cell);
+        player[clickedCellValue] = 1;
+        // currentPlayer(playerO_turn)
+
+        if (clickCounter >=3){
+            checkForWinner()
+        };
+    } else {
+        clickedCell.innerText = playerO;
+        clickedCell.dataset.taken = "true";
+        clickCounter += 1;
+        playerO_turn = false;
+        let clickedCellValue = parseInt(clickedCell.dataset.cell);
+        player[clickedCellValue] = 2;
+        // currentPlayer(playerO_turn)
+
+        if (clickCounter >=3){
+            checkForWinner()
+        };
+    }
+
+    clickSound.play();
+    gameEnd();
+
+}
+
+
+// event listeners
+board.addEventListener("mouseover", hoverText)
+board.addEventListener("click",main)
 restartButton.addEventListener("click",restartGame)
